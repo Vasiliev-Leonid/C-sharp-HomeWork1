3a13efddcd6ae883f23e0d77a5f722d1c6962651
Leonid Vassiliev <me2021@tut.by>
2022-10-18 10:53:56 +0300

    add program for HomeWork1

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 2. Напишите программу, которая на вход принимает два числа и выдает какое число больше, а какое меньше. 
+//Console.WriteLine("Input a number : ");
+//int numa = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Input b number : ");
+//int numb = Convert.ToInt32(Console.ReadLine());
+//if (numa>numb){
+//    Console.WriteLine($"{numa} Is the max number ");
+//}
+//else{
+//Console.WriteLine($"{numb} Is the max number ");  
+//}  
+// Задача 4. Напишите программу, которая на вход принимает три числа и выдает максимальное из этих чисел
+//Console.WriteLine("Input first number : ");
+//int num1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Input second number : ");
+//int num2 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Input third number : ");
+//int num3 = Convert.ToInt32(Console.ReadLine());
+//if (num1 > num2 && num1 > num3){
+//    Console.WriteLine($"{num1} is the max number ");
+//}
+//else;
+// Задача 6. Напишите программу, которая на вход принимает  число и выдает является ли число четным (делится на 2 без остатка)
+//Console.WriteLine("Input first number : ");
+//int num = Convert.ToInt32(Console.ReadLine());
+//if (num % 2 == 0){
+//    Console.WriteLine($"{num} Четное ");
+//}
+//else {
+//    Console.WriteLine($"{num} Нечетное ");
+//}
+// Задача 8 . Напишите программу, которая на вход принимает  число  (N) , а на выходе показывает все четные числа от 1 до N
+Console.WriteLine("Input number : ");
+int num = Convert.ToInt32(Console.ReadLine());
+//for (int i = 1; i <= num; i++){
+//if (i % 2 == 0)
+//    Console.WriteLine(i + " ");
+//}    
